0a1d79131a5418d3d11875fdaf597b1517264586
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2025-03-10 23:55:52 -0300

    Add env var to set gunicorn timeout

--- {{ cookiecutter.project_slug }}/bin/web.sh
+++ {{ cookiecutter.project_slug }}/bin/web.sh
@@ -13,9 +13,12 @@ fi
 if [ -z ${GUNICORN_WORKERS+x} ]; then
   GUNICORN_WORKERS={{ cookiecutter.gunicorn_workers }}
 fi
+if [ -z ${WEB_TIMEOUT+x} ]; then
+  WEB_TIMEOUT=300
+fi
 
 HOST_PORT="0.0.0.0:$PORT"
-OPTS="--bind=$HOST_PORT --chdir=/app --log-file - --access-logfile - --workers=$GUNICORN_WORKERS"
+OPTS="--bind=$HOST_PORT --chdir=/app --log-file - --access-logfile - --workers=$GUNICORN_WORKERS --timeout=$WEB_TIMEOUT"
 if [[ "$(echo $USE_ASGI | tr a-z A-Z)" = "TRUE" ]]; then
   APP_MODULE="project.asgi:application"
   OPTS="$OPTS --worker-class uvicorn.workers.UvicornWorker"
