8823dd6536de1f8538cae247178bb9b48538da08
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2025-10-29 18:34:10 -0300

    Fix variable name in conftest

--- {{ cookiecutter.project_slug }}/conftest.py
+++ {{ cookiecutter.project_slug }}/conftest.py
@@ -7,6 +7,6 @@ def pytest_collect_file(filename, parent):
     """Hook to intercept pytest file collect"""
     config = str(os.environ.get("PYTEST_DEBUG_COLLECT") or "").strip().lower()
     if config in ("true", "on", "1"):
-        print(f"Collecting tests from {file_path}")
+        print(f"Collecting tests from {filename}")
 
     return None  # Will continue the regular collect mechanism
