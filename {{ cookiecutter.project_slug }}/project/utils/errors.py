71baff07e639a9551199766a5336f4a6f7d60743
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2026-04-23 20:41:31 -0300

    Move `sentry_sdk` imports into `report_error`
    The file was generated regardless of the `enable_sentry` choice, but
    `sentry_sdk` only got added to the dependencies when `enable_sentry=y`,
    which would generate an error in pytest test collection phase.

--- {{ cookiecutter.project_slug }}/project/utils/errors.py
+++ {{ cookiecutter.project_slug }}/project/utils/errors.py
@@ -1,10 +1,14 @@
-import json
-import sys
-
-from sentry_sdk import Scope, capture_message
-
-
 def report_error(message: str, context: dict, level: str):
+    """Report an error to Sentry (if configured) and stderr.
+
+    The `message` should describe the class of error, without object IDs or other specifics, so that Sentry groups all
+    errors of the same type. Put object-specific details in `context`.
+    """
+    import json
+    import sys
+
+    from sentry_sdk import Scope, capture_message
+
     scope = Scope()
     scope.set_context("context", context)
     capture_message(message, level=level, scope=scope)
